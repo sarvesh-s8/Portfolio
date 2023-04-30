e0fb1a4aaecf7f84d205e32b1a05a083ec2f500f
sarvesh-samant-au9 <sarveshsamant1140@gmail.com>
2023-04-29 20:28:15 -0400

    Updates

---
+++ precache-manifest.fe38367824971d91dfb36c718d851c64.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "29ba86efd674a6ebc8d4",
+    "url": "/static/css/main.28d41647.chunk.css"
+  },
+  {
+    "revision": "29ba86efd674a6ebc8d4",
+    "url": "/static/js/main.29ba86ef.chunk.js"
+  },
+  {
+    "revision": "3e566464e97eca92cb39",
+    "url": "/static/css/1.566231d6.chunk.css"
+  },
+  {
+    "revision": "3e566464e97eca92cb39",
+    "url": "/static/js/1.3e566464.chunk.js"
+  },
+  {
+    "revision": "edce810c5cea0d95ff77",
+    "url": "/static/js/2.edce810c.chunk.js"
+  },
+  {
+    "revision": "be93153dd4875e6fe677",
+    "url": "/static/js/runtime~main.be93153d.js"
+  },
+  {
+    "revision": "a15c230b794fda4eb28f2320529072a1",
+    "url": "/static/media/manOnTable.a15c230b.svg"
+  },
+  {
+    "revision": "a41f784840703e7c39e9d5ff37133c77",
+    "url": "/static/media/developerActivity.a41f7848.svg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "d6a2dcd5cb04a2d44cca99b6acb5da7e",
+    "url": "/static/media/ibm_logo.d6a2dcd5.png"
+  },
+  {
+    "revision": "b1ae2adc77bafd0ab3f670d9ab06b5ca",
+    "url": "/static/media/jsFramework.b1ae2adc.svg"
+  },
+  {
+    "revision": "f041d99793f0c15a757a3d8b73d937a8",
+    "url": "/static/media/pierian-data-logo.f041d997.png"
+  },
+  {
+    "revision": "96c9d7a913fc06de9bd7e13f5cb26af9",
+    "url": "/static/media/skill.96c9d7a9.svg"
+  },
+  {
+    "revision": "6ae21d5c3abe67f6adcefd19ce8fa3f0",
+    "url": "/index.html"
+  }
+];
